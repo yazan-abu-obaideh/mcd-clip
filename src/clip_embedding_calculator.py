806de072492155cebe6282d9fa9884168f9c6d27
Yazan Amer <yazan.amer@progressoft.com>
2024-01-23 14:04:13 +0300

    feat: clip_embedding_calculator.py

--- src/clip_embedding_calculator.py
+++ src/clip_embedding_calculator.py
@@ -1,5 +1,7 @@
 import abc
 
+import numpy as np
+from PIL import Image
 from transformers import CLIPProcessor, CLIPTokenizerFast, CLIPModel
 
 _DEVICE = "cuda"
@@ -11,24 +13,24 @@ _MODEL = CLIPModel.from_pretrained(_MODEL_ID)  # .to(device)
 
 class ClipEmbeddingCalculator(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def from_text(self, text: str):
+    def from_text(self, text: str) -> np.ndarray:
         pass
 
     @abc.abstractmethod
-    def from_image_bytes(self, image_bytes: bytes):
-        pass
-
-    @abc.abstractmethod
-    def from_image_path(self, image_path: str):
+    def from_image_path(self, image_path: str) -> np.ndarray:
         pass
 
 
 class ClipEmbeddingCalculatorImpl(ClipEmbeddingCalculator):
-    def from_text(self, text: str):
+    def from_text(self, text: str) -> np.ndarray:
         return _MODEL.get_text_features(**_TOKENIZER(text, return_tensors="pt")).detach().numpy()
 
-    def from_image_bytes(self, image_bytes: bytes):
-        pass
-
-    def from_image_path(self, image_path: str):
-        pass
+    def from_image_path(self, image_path: str) -> np.ndarray:
+        img = Image.open(image_path).convert("RGB")
+        width, height = img.size
+        img = img.resize((width // 2, height // 2))
+        result = Image.new(img.mode, (1300, 1300), (255, 255, 255))
+        result.paste(img, img.getbbox())
+        image = np.asarray(result)
+        img_processed = _CLIP_PROCESSOR(text=None, images=image, return_tensors='pt')['pixel_values']  # .to(device)
+        return _MODEL.get_image_features(img_processed).detach().numpy()
--- test/test_clip_embedding_calculator.py
+++ test/test_clip_embedding_calculator.py
@@ -1,8 +1,11 @@
+import os.path
 import unittest
 
 import numpy as np
 
 from clip_embedding_calculator import ClipEmbeddingCalculatorImpl
+
+BIKE_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "resources", "black_bicycle.jpeg")
 
 
 class ClipEmbeddingCalculatorTest(unittest.TestCase):
@@ -10,6 +13,11 @@ class ClipEmbeddingCalculatorTest(unittest.TestCase):
         self.embedding_calculator = ClipEmbeddingCalculatorImpl()
 
     def test_embedding_from_text(self):
-        embedding = self.embedding_calculator.from_text("Black bike")
+        self.assertValidEmbedding(self.embedding_calculator.from_text("Black bike"))
+
+    def test_embedding_from_image_path(self):
+        self.assertValidEmbedding(self.embedding_calculator.from_image_path(BIKE_IMAGE_PATH))
+
+    def assertValidEmbedding(self, embedding):
         self.assertTrue(type(embedding) is np.ndarray)
         self.assertEqual((1, 512), embedding.shape)
