559296247d63eb7c03c776cb77e1e44e21ae5a5e
Yazan Amer <yazan.amer@progressoft.com>
2024-01-23 14:35:48 +0300

    refactor: embedding_similarity_optimizer.py

--- src/embedding_similarity_optimizer.py
+++ src/embedding_similarity_optimizer.py
@@ -4,11 +4,15 @@ import numpy as np
 import pandas as pd
 from decode_mcd import DesignTargets, DataPackage, MultiObjectiveProblem, CounterfactualsGenerator, ContinuousTarget
 
+import clip_embedding_calculator
 from datatypes_mapper import map_column
 from embedding_comparator import get_cosine_similarity
 from embedding_predictor import EmbeddingPredictor
 
 PREDICTOR = EmbeddingPredictor()
+
+CONSTANT_COLUMNS = ['Wall thickness Bottom Bracket', 'Wall thickness Top tube',
+                    'Wall thickness Head tube', 'Wall thickness Down tube']
 
 
 def get_features():
@@ -22,6 +26,8 @@ def get_labels(target_embedding: np.ndarray):
 
 
 def predict(designs, target_embedding):
+    for column in CONSTANT_COLUMNS:
+        designs[column] = get_features()[column].mean()
     return 1 - get_cosine_similarity(PREDICTOR.predict(designs), target_embedding)
 
 
@@ -29,18 +35,19 @@ def map_datatypes():
     datatypes = []
     features = get_features()
     for column_name in list(features.columns):
-        datatypes.append(map_column(features[column_name]))
+        if column_name not in CONSTANT_COLUMNS:
+            datatypes.append(map_column(features[column_name]))
     return datatypes
 
 
 def do_problem(target_embedding: np.ndarray):
-    data_package = DataPackage(features_dataset=get_features(),
+    data_package = DataPackage(features_dataset=get_features().drop(columns=CONSTANT_COLUMNS),
                                predictions_dataset=pd.DataFrame(get_labels(target_embedding),
                                                                 columns=["cosine_distance"]),
-                               query_x=get_features().get(1),
+                               query_x=get_features().iloc[0:1].drop(columns=CONSTANT_COLUMNS),
                                design_targets=DesignTargets([ContinuousTarget(label="cosine_distance",
                                                                               lower_bound=0,
-                                                                              upper_bound=1)]),
+                                                                              upper_bound=0.2)]),
                                datatypes=map_datatypes(),
                                bonus_objectives=["cosine_distance"])
 
@@ -60,4 +67,5 @@ def do_problem(target_embedding: np.ndarray):
 
 
 if __name__ == "__main__":
-    pass
+    embedding_calculator = clip_embedding_calculator.ClipEmbeddingCalculatorImpl()
+    do_problem(embedding_calculator.from_text("Black bicycle").reshape((512,)))
