f5d3e07933e22498b2a59e1d385055cbd4a1f848
Yazan Amer <yazan.amer@progressoft.com>
2024-01-24 11:45:35 +0300

    feat: embedding_similarity_optimizer.py

--- src/profile_optimizer.py
+++ src/profile_optimizer.py
@@ -7,8 +7,7 @@ from embedding_similarity_optimizer import do_problem
 if __name__ == "__main__":
     embedding_calculator = clip_embedding_calculator.ClipEmbeddingCalculatorImpl()
     target_text = "Black bicycle"
-    cProfile.run("""
-    do_problem(embedding_calculator.from_text(target_text).reshape((512,)),
+    cProfile.run("""do_problem(embedding_calculator.from_text(target_text).reshape((512,)),
                      pop_size=2000,
                      n_generations=20,
                      initialize_from_dataset=True
