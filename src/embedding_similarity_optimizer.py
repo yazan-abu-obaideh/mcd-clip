6db761fc48977eb852a7287a1936a75a6a4bfde2
Yazan Amer <yazan.amer@progressoft.com>
2024-01-24 11:06:28 +0300

    feat: embedding_similarity_optimizer.py

--- src/embedding_similarity_optimizer.py
+++ src/embedding_similarity_optimizer.py
@@ -20,34 +20,39 @@ def get_features():
                        index_col=0)
 
 
+FEATURES = get_features()
+
+
 def get_labels(target_embedding: np.ndarray):
-    predictions = PREDICTOR.predict(get_features())
+    predictions = PREDICTOR.predict(FEATURES)
     return 1 - get_cosine_similarity(predictions, target_embedding)
 
 
 def predict(designs, target_embedding):
     designs_copy = designs.copy().astype("float64")
     for column in CONSTANT_COLUMNS:
-        designs_copy[column] = get_features()[column].mean()
+        designs_copy[column] = FEATURES[column].mean()
     return 1 - get_cosine_similarity(PREDICTOR.predict(designs_copy), target_embedding)
 
 
 def map_datatypes():
     datatypes = []
-    features = get_features()
-    for column_name in list(features.columns):
+    for column_name in list(FEATURES.columns):
         if column_name not in CONSTANT_COLUMNS:
-            datatypes.append(map_column(features[column_name]))
+            datatypes.append(map_column(FEATURES[column_name]))
     return datatypes
 
 
-def do_problem(target_embedding: np.ndarray, pop_size=1000, n_generations=30, initialize_from_dataset=False):
-    features_dataset = get_features().drop(columns=CONSTANT_COLUMNS)
+def do_problem(target_embedding: np.ndarray,
+               pop_size=1000,
+               n_generations=30,
+               initialize_from_dataset=False):
+    features_dataset = FEATURES.drop(columns=CONSTANT_COLUMNS)
     data_package = DataPackage(features_dataset=features_dataset,
                                predictions_dataset=pd.DataFrame(get_labels(target_embedding),
                                                                 columns=["cosine_distance"],
                                                                 index=features_dataset.index),
-                               query_x=get_features().iloc[0:1].drop(columns=CONSTANT_COLUMNS),
+                               query_x=FEATURES.iloc[0:1].drop(columns=CONSTANT_COLUMNS),
                                design_targets=DesignTargets([ContinuousTarget(label="cosine_distance",
                                                                               lower_bound=0,
                                                                               upper_bound=0.2)]),
@@ -65,11 +70,17 @@ def do_problem(target_embedding: np.ndarray, pop_size=1000, n_generations=30, in
     generator.generate(n_generations=n_generations)
     return generator.sample_with_dtai(num_samples=10, gower_weight=1,
                                       avg_gower_weight=1, cfc_weight=1,
-                                      diversity_weight=50)
+                                      diversity_weight=50,
+                                      include_dataset=False)
 
 
 if __name__ == "__main__":
     embedding_calculator = clip_embedding_calculator.ClipEmbeddingCalculatorImpl()
-    cfs = do_problem(embedding_calculator.from_text("Black bicycle").reshape((512,)))
-    with open("result.csv", "w") as file:
+    target_text = "Black bicycle"
+    cfs = do_problem(embedding_calculator.from_text(target_text).reshape((512,)),
+                     pop_size=2000,
+                     n_generations=100,
+                     initialize_from_dataset=True
+                     )
+    with open(f"{target_text}_result.csv", "w") as file:
         cfs.to_csv(file)
