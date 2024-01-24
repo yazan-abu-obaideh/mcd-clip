cbcd11b04108197dafd94fbdf5771e6331227f37
Yazan Amer <yazan.amer@progressoft.com>
2024-01-24 11:44:59 +0300

    feat: embedding_similarity_optimizer.py

---
+++ src/profile_optimizer.py
@@ -0,0 +1,16 @@
+import cProfile
+
+import clip_embedding_calculator
+# noinspection PyUnresolvedReferences
+from embedding_similarity_optimizer import do_problem
+
+if __name__ == "__main__":
+    embedding_calculator = clip_embedding_calculator.ClipEmbeddingCalculatorImpl()
+    target_text = "Black bicycle"
+    cProfile.run("""
+    do_problem(embedding_calculator.from_text(target_text).reshape((512,)),
+                     pop_size=2000,
+                     n_generations=20,
+                     initialize_from_dataset=True
+                     )
+    """, filename="run-results/profiling_results.txt")
