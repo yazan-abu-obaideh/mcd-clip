e943139c4dd53241c08e4879f8a4eb5d2ca71f40
Yazan Amer <yazan.amer@progressoft.com>
2024-01-26 11:27:04 +0300

    feat: embedding_similarity_optimizer.py

--- test/test_datatypes_mapper.py
+++ test/test_datatypes_mapper.py
@@ -8,30 +8,26 @@ from datatypes_mapper import map_column
 
 class MapperTest(unittest.TestCase):
     def test_float_mapping(self):
-        series = pd.Series()
+        series = pd.Series(data=[15, 25], dtype="float64")
         series.name = "Head angle"
-        series[0] = 15
-        series[1] = 25
         column_type = map_column(series)
         self.assertTrue(type(column_type) is Real)
         self.assertEqual(15, column_type.bounds[0])
         self.assertEqual(25, column_type.bounds[1])
 
     def test_integer_mapping(self):
-        series = pd.Series()
+        series = pd.Series(data=[15, 25], dtype="int64")
         series.name = "Number of chainrings"
-        series[0] = 15
-        series[1] = 25
         column_type = map_column(series)
         self.assertTrue(type(column_type) is Integer)
         self.assertEqual(15, column_type.bounds[0])
         self.assertEqual(25, column_type.bounds[1])
 
     def test_bool_mapping(self):
-        series = pd.Series()
+        series = pd.Series(data=[False, True], dtype="bool")
         series.name = "CHAINSTAYbrdgCheck"
-        series[0] = False
-        series[1] = True
+        # series[0] = False
+        # series[1] = True
         series = series.astype(dtype="bool")
         mapped_type = map_column(series)
         self.assertTrue(type(mapped_type) is Choice)
