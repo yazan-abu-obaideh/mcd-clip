0c702910b5a91a0312e48171add3265dd086b101
Yazan Amer <yazan.amer@progressoft.com>
2024-02-07 08:39:17 +0300

    feat: BIKED performance prediction

---
+++ src/mcd_clip/biked/load_data.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 10 12:05:43 2022
+
+@author: Lyle
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+from mcd_clip.resource_utils import resource_path
+
+ALL_STRUCTURAL_DATASET = resource_path('all_structural_data_aug.csv')
+
+
+def one_hot_encode_material(data):
+    data = data.copy()
+    # One-hot encode the materials
+    data["Material"] = pd.Categorical(data["Material"], categories=["Steel", "Aluminum", "Titanium"])
+    mats_oh = pd.get_dummies(data["Material"], prefix="Material=", prefix_sep="")
+    data.drop(["Material"], axis=1, inplace=True)
+    data = pd.concat([mats_oh, data], axis=1)
+    return data
+
+
+def load_augmented_framed_dataset(to_magnitudes=False) -> (
+        tuple)[pd.DataFrame, pd.DataFrame, StandardScaler, StandardScaler]:
+    reg_data = pd.read_csv(ALL_STRUCTURAL_DATASET, index_col=0)
+
+    x = reg_data.iloc[:, :-11]
+
+    x = one_hot_encode_material(x)
+
+    x, x_scaler = scale(x)
+    y = reg_data.iloc[:, -11:-1]
+
+    for col in ['Sim 1 Safety Factor', 'Sim 3 Safety Factor']:
+        y[col] = 1 / y[col]
+        y.rename(columns={col: col + " (Inverted)"}, inplace=True)
+
+    convert_to_magnitudes(y, to_magnitudes)
+
+    y, y_scaler = scale(y)
+
+    return x, y, x_scaler, y_scaler
+
+
+def convert_to_magnitudes(y, to_magnitudes):
+    if to_magnitudes:
+        for col in ['Sim 1 Dropout X Disp.', 'Sim 1 Dropout Y Disp.', 'Sim 1 Bottom Bracket X Disp.',
+                    'Sim 1 Bottom Bracket Y Disp.', 'Sim 2 Bottom Bracket Z Disp.', 'Sim 3 Bottom Bracket Y Disp.',
+                    'Sim 3 Bottom Bracket X Rot.', 'Model Mass']:
+            y[col] = [np.abs(val) for val in y[col].values]
+            y.rename(columns={col: col + " Magnitude"}, inplace=True)
+
+
+def scale(v):
+    v_scaler = StandardScaler()
+    v_scaler.fit(v)
+    v_scaled_values = v_scaler.transform(v)
+    new_v = pd.DataFrame(v_scaled_values, columns=v.columns, index=v.index)
+    return new_v, v_scaler
---
+++ src/mcd_clip/biked/structural_predictor.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from mcd_clip.biked.MultilabelPredictor import MultilabelPredictor
+from mcd_clip.resource_utils import resource_path
+import __main__
+
+_MODEL_PATH = resource_path('generated/AutogluonModels/ag-20231016_092811')
+
+
+class StructuralPredictor:
+    def __init__(self):
+        __main__.MultilabelPredictor = MultilabelPredictor
+        self._predictor = MultilabelPredictor.load(_MODEL_PATH)
+
+    def predict(self, x: pd.DataFrame):
+        return self._predictor.predict(x)
