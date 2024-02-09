91ea2f337727df18d304e7138187819e9e47acbe
Yazan Amer <yazan.amer@progressoft.com>
2024-02-09 16:27:53 +0300

    feat: combined_optimizer.py

--- src/mcd_clip/combined_optimization/columns_constants.py
+++ src/mcd_clip/combined_optimization/columns_constants.py
@@ -1,7 +1,7 @@
 IDENTICAL_FRAMED = ['HT Angle', 'SSB_Include', 'ST Angle', 'CSB_Include']
 IDENTICAL_CLIPS = ['Head angle', 'SEATSTAYbrdgCheck', 'Seat angle', 'CHAINSTAYbrdgCheck']
 
-SCALED_BIKED = ['SS OD', 'HT Length', 'CS OD', 'ST UX', 'HT LX', 'HT UX', 'ST Length', 'BB Drop', 'CSB Offset', 'SS E',
+SCALED_FRAMED = ['SS OD', 'HT Length', 'CS OD', 'ST UX', 'HT LX', 'HT UX', 'ST Length', 'BB Drop', 'CSB Offset', 'SS E',
                 'Dropout Offset', 'SS Z', 'Stack', 'CS Length', 'DT Length', 'HT OD', 'SSB Offset', 'BB OD', 'DT OD',
                 'TT OD', 'BB Length', 'ST OD']
 SCALED_CLIPS = ['ssd', 'Head tube length textfield', 'csd', 'Seat tube extension2', 'Head tube lower extension2',
