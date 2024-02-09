cbc45e14d6c40787a9610b214ff2e350facee5cd
Yazan Amer <yazan.amer@progressoft.com>
2024-02-09 16:27:59 +0300

    feat: combined_optimizer.py

--- src/mcd_clip/combined_optimization/columns_constants.py
+++ src/mcd_clip/combined_optimization/columns_constants.py
@@ -2,8 +2,8 @@ IDENTICAL_FRAMED = ['HT Angle', 'SSB_Include', 'ST Angle', 'CSB_Include']
 IDENTICAL_CLIPS = ['Head angle', 'SEATSTAYbrdgCheck', 'Seat angle', 'CHAINSTAYbrdgCheck']
 
 SCALED_FRAMED = ['SS OD', 'HT Length', 'CS OD', 'ST UX', 'HT LX', 'HT UX', 'ST Length', 'BB Drop', 'CSB Offset', 'SS E',
-                'Dropout Offset', 'SS Z', 'Stack', 'CS Length', 'DT Length', 'HT OD', 'SSB Offset', 'BB OD', 'DT OD',
-                'TT OD', 'BB Length', 'ST OD']
+                 'Dropout Offset', 'SS Z', 'Stack', 'CS Length', 'DT Length', 'HT OD', 'SSB Offset', 'BB OD', 'DT OD',
+                 'TT OD', 'BB Length', 'ST OD']
 SCALED_CLIPS = ['ssd', 'Head tube length textfield', 'csd', 'Seat tube extension2', 'Head tube lower extension2',
                 'Head tube upper extension2', 'Seat tube length', 'BB textfield', 'CHAINSTAYbrdgshift',
                 'Seat stay junction0', 'Dropout spacing', 'SSTopZOFFSET', 'Stack', 'CS textfield', 'DT Length',
