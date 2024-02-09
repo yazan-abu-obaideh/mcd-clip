7cd656e416c1bf1da2d27a1da68d212f682fac13
Yazan Amer <yazan.amer@progressoft.com>
2024-02-09 23:01:55 +0300

    feat: combined_optimizer.py

--- src/mcd_clip/combined_optimization/columns_constants.py
+++ src/mcd_clip/combined_optimization/columns_constants.py
@@ -1,11 +1,10 @@
-IDENTICAL_FRAMED = ['HT Angle', 'SSB_Include', 'ST Angle', 'CSB_Include']
-IDENTICAL_CLIPS = ['Head angle', 'SEATSTAYbrdgCheck', 'Seat angle', 'CHAINSTAYbrdgCheck']
-
-SCALED_FRAMED = ['SS OD', 'HT Length', 'CS OD', 'ST UX', 'HT LX', 'HT UX', 'ST Length', 'BB Drop', 'CSB Offset', 'SS E',
-                 'Dropout Offset', 'SS Z', 'Stack', 'CS Length', 'DT Length', 'HT OD', 'SSB Offset', 'BB OD', 'DT OD',
-                 'TT OD', 'BB Length', 'ST OD']
-SCALED_CLIPS = ['ssd', 'Head tube length textfield', 'csd', 'Seat tube extension2', 'Head tube lower extension2',
-                'Head tube upper extension2', 'Seat tube length', 'BB textfield', 'CHAINSTAYbrdgshift',
-                'Seat stay junction0', 'Dropout spacing', 'SSTopZOFFSET', 'Stack', 'CS textfield', 'DT Length',
-                'Head tube diameter', 'SEATSTAYbrdgshift', 'BB diameter', 'dtd', 'ttd', 'BB length',
-                'Seat tube diameter']
+FRAMED_TO_CLIPS_UNITS = {'SS OD': 'ssd', 'HT Length': 'Head tube length textfield', 'CS OD': 'csd',
+                         'ST UX': 'Seat tube extension2', 'HT LX': 'Head tube lower extension2',
+                         'HT UX': 'Head tube upper extension2', 'ST Length': 'Seat tube length',
+                         'BB Drop': 'BB textfield', 'CSB Offset': 'CHAINSTAYbrdgshift', 'SS E': 'Seat stay junction0',
+                         'Dropout Offset': 'Dropout spacing', 'SS Z': 'SSTopZOFFSET', 'Stack': 'Stack',
+                         'CS Length': 'CS textfield', 'DT Length': 'DT Length', 'HT OD': 'Head tube diameter',
+                         'SSB Offset': 'SEATSTAYbrdgshift', 'BB OD': 'BB diameter', 'DT OD': 'dtd', 'TT OD': 'ttd',
+                         'BB Length': 'BB length', 'ST OD': 'Seat tube diameter'}
+FRAMED_TO_CLIPS_IDENTICAL = {'HT Angle': 'Head angle', 'SSB_Include': 'SEATSTAYbrdgCheck', 'ST Angle': 'Seat angle',
+                             'CSB_Include': 'CHAINSTAYbrdgCheck'}
--- test/test_datasets_merge.py
+++ test/test_datasets_merge.py
@@ -5,79 +5,85 @@ import numpy as np
 import numpy.testing as np_test
 import pandas as pd
 
+from mcd_clip.biked.load_data import load_augmented_framed_dataset
 from mcd_clip.resource_utils import resource_path
 
 
 class DatasetMergeTest(unittest.TestCase):
     def setUp(self):
-        self.biked = pd.read_csv(resource_path('all_structural_data_aug.csv'), index_col=0).drop(columns=['batch'])
+        self.framed, y, x_scaler, y_scaler = load_augmented_framed_dataset()
+        self.framed = pd.DataFrame(x_scaler.inverse_transform(self.framed),
+                                   columns=self.framed.columns,
+                                   index=self.framed.index)
         self.clips = pd.read_csv(resource_path('clip_sBIKED_processed.csv'), index_col=0)
 
     def test_outliers(self):
-        for column in self.biked.columns:
-            print(self.biked[column].describe())
+        for column in self.framed.columns:
+            print(self.framed[column].describe())
 
     def test_dataset_len(self):
-        self.assertEqual((len(self.biked), len(self.clips)), (14851, 4512))
+        self.assertEqual((len(self.framed), len(self.clips)), (14851, 4512))
 
     def test_intersection_len(self):
         self.assertEqual(len(self._get_index_intersection()), 4046)
 
     def test_intersection_columns(self):
-        b_columns = set(self.biked.columns)
+        b_columns = set(self.framed.columns)
         c_columns = set(self.clips.columns)
         self.assertEqual(b_columns.intersection(c_columns), {'DT Length', 'Stack'})
 
         indices = self._get_index_intersection()
         clips_indices = [int(idx) for idx in indices]
-        biked_subset = self.biked.loc[indices, :]
+        framed_subset = self.framed.loc[indices, :]
         clips_subset = self.clips.loc[clips_indices, :]
 
         np_test.assert_array_almost_equal(
-            biked_subset[['DT Length', 'Stack']].values * 1000,
+            framed_subset[['DT Length', 'Stack']].values * 1000,
             clips_subset[['DT Length', 'Stack']].values,
             decimal=2
         )
 
     def test_columns_with_identical_values(self):
-        b_columns = set(self.biked.columns)
+        b_columns = set(self.framed.columns)
         c_columns = set(self.clips.columns)
         indices = self._get_index_intersection()
         clip_indices = [int(idx) for idx in indices]
-        b_columns = {c for c in b_columns if c not in ['Material']}
-        identical_biked = []
+        identical_framed = []
         identical_clips = []
-        scaled_biked = []
+        scaled_framed = []
         scaled_clips = []
         for b_column in b_columns:
             for c_column in c_columns:
                 try:
                     if (np.isclose(
-                            self.biked[b_column].loc[indices].values,
+                            self.framed[b_column].loc[indices].values,
                             self.clips[c_column].loc[clip_indices].values,
                             atol=1e-2
                     ).all()):
                         print(f"{b_column} has very close values to {c_column}")
-                        identical_biked.append(b_column)
+                        identical_framed.append(b_column)
                         identical_clips.append(c_column)
                     elif np.isclose(
-                            self.biked[b_column].loc[indices].values * 1000,
+                            self.framed[b_column].loc[indices].values * 1000,
                             self.clips[c_column].loc[clip_indices].values,
                             atol=1e-2
                     ).all():
-                        scaled_biked.append(b_column)
+                        scaled_framed.append(b_column)
                         scaled_clips.append(c_column)
                         print(f"{b_column} when scaled has very close values to {c_column}")
                 except Exception as e:
                     print(f"Exception occurred because of {b_column} and {c_column}")
                     raise e
 
-        print(f"{identical_biked=}")
-        print(f"{identical_clips=}")
-        print(f"{scaled_biked=}")
+        print(f"{scaled_framed=}")
         print(f"{scaled_clips=}")
+        print(f"{identical_framed=}")
+        print(f"{identical_clips=}")
+
+        self.assertEqual(len(scaled_framed), len(scaled_clips))
+        self.assertEqual(len(identical_framed), len(identical_clips))
 
     def _get_index_intersection(self) -> List[str]:
-        biked_idx_set = set(self.biked.index)
+        framed_idx_set = set(self.framed.index)
         clips_idx_set = set([str(index) for index in self.clips.index])
-        return list(biked_idx_set.intersection(clips_idx_set))
+        return list(framed_idx_set.intersection(clips_idx_set))
