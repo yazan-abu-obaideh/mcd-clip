a33c9bcda96825d77e9b7a914d99dadaa850a6e8
Yazan Amer <yazan.amer@progressoft.com>
2024-02-20 15:33:04 +0300

    feat: 2D ablation study

--- src/mcd_clip/template_runs/simple_ablation.py
+++ src/mcd_clip/template_runs/simple_ablation.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from decode_mcd import data_package, MultiObjectiveProblem, CounterfactualsGenerator, ContinuousTarget, DesignTargets, \
+from decode_mcd import MultiObjectiveProblem, CounterfactualsGenerator, ContinuousTarget, DesignTargets, \
     DataPackage
 from pymoo.core.variable import Real
 
 
 class SimpleAblation2Dimensions:
     def run_case_study(self):
-        self._draw_valid_region()
         self._run_without_features()
         self._run_with_features()
 
+    def secret_validity(self, points: pd.DataFrame):
+        distance_ = np.sum(np.power(points.values, 2), axis=1)
+        return np.logical_and(distance_ < 1, distance_ > 0.9)
+
     def _run_without_features(self):
-        pass
+        self._do_2d_case(ablation=True)
 
     def _run_with_features(self):
-        pass
+        self._do_2d_case(ablation=False)
 
     def _run(self, features: bool):
         pass
@@ -24,37 +27,14 @@ class SimpleAblation2Dimensions:
     def validity(self, _x):  # Validity function for the 2D case
         a = _x["X"]  # Separate the two dimensions for clarity
         b = _x["Y"]
-        fc = np.less(np.sqrt(np.power((a - 0.3), 2) + np.power((b - 0.3), 2)), 0.1)  # Circle
-        sc = np.less(np.power(np.power(np.power((a - b), 6) - 1, 2) + np.power(np.power((a + b), 6) - 1, 2), 2),
-                     0.99)  # Arcs
-        return np.logical_or(fc, sc)  # If points are in circle or arcs they are valid
+        point_in_arcs = np.less(
+            np.power(np.power(np.power((a - b), 6) - 1, 2) + np.power(np.power((a + b), 6) - 1, 2), 2), 0.99)
+        return point_in_arcs
 
     def _draw_arcs(self):
         pass
 
-    def plotcfs(self, validity, counterfactuals, query, rangearr, dataset=False):
-        xx, yy = np.mgrid[rangearr[0, 0]:rangearr[0, 1]:.001, rangearr[1, 0]:rangearr[1, 1]:.001]
-        grid = np.c_[xx.ravel(), yy.ravel()]
-        grid_df = pd.DataFrame(grid, columns=["X", "Y"])
-        Z = validity(grid_df)
-        Z = np.array(Z)
-        Z = Z.reshape(xx.shape)
-        fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=200)
-        img = ax.imshow(1 - Z.T, cmap="gray", alpha=0.5, origin='lower', extent=rangearr.flatten())
-        ax.axis('off')
-
-        ax.scatter(query[0], query[1], s=100, c="k", alpha=1, marker="x")
-        ax.annotate("Query", (query[0] - .15, query[0] + 0.1))
-        if not dataset:
-            ax.scatter(counterfactuals[:, 0], counterfactuals[:, 1], s=100, c="k", alpha=1, marker=".")
-            for i in range(np.shape(counterfactuals)[0]):
-                ax.plot([query[0], counterfactuals[i, 0]], [query[1], counterfactuals[i, 1]], c="k", linestyle="--",
-                        lw=1, alpha=0.5)
-        else:
-            ax.scatter(counterfactuals[:, 0], counterfactuals[:, 1], s=1, c="k", alpha=1, marker=".")
-
-    def _draw_valid_region(self):
-        # Define the radii of the circles
+    def _draw_circles(self):
         radii = [5, 4, 3, 2, 1]
 
         # Define the colors for the circles
@@ -75,16 +55,13 @@ class SimpleAblation2Dimensions:
         ax.set_xlim(-max(radii) - 1, max(radii) + 1)
         ax.set_ylim(-max(radii) - 1, max(radii) + 1)
 
-        self._do_2d_case()
-
         plt.savefig('circles.png')
 
-    def plotcfs(self,
-                validity,
-                counterfactuals,
-                query,
-                rangearr,
-                dataset=False):
+    def plot_cfs(self,
+                 counterfactuals,
+                 query,
+                 rangearr,
+                 dataset=False):
         xx, yy = np.mgrid[rangearr[0, 0]:rangearr[0, 1]:.001, rangearr[1, 0]:rangearr[1, 1]:.001]
         grid = np.c_[xx.ravel(), yy.ravel()]
         grid_df = pd.DataFrame(grid, columns=["X", "Y"])
@@ -92,7 +69,8 @@ class SimpleAblation2Dimensions:
         Z = np.array(Z)
         Z = Z.reshape(xx.shape)
         fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=200)
-        img = ax.imshow(1 - Z.T, cmap="gray", alpha=0.5, origin='lower', extent=rangearr.flatten())
+        plt.Circle((0, 0), 0.5, color='blue', alpha=0.5)
+        ax.imshow(1 - Z.T, cmap="gray", alpha=0.5, origin='lower', extent=rangearr.flatten())
         ax.axis('off')
 
         ax.scatter(query[0], query[1], s=100, c="k", alpha=1, marker="x")
@@ -105,12 +83,19 @@ class SimpleAblation2Dimensions:
         else:
             ax.scatter(counterfactuals[:, 0], counterfactuals[:, 1], s=1, c="k", alpha=1, marker=".")
 
-    def _do_2d_case(self):
+        circle = plt.Circle((0, 0), 1, color='red', alpha=0.5)
+        ax.add_artist(circle)
+        circle = plt.Circle((0, 0), 0.95, color='white', alpha=0.5)
+        ax.add_artist(circle)
+
+    def _do_2d_case(self, ablation: bool):
         all_datapoints = np.random.rand(10000, 2)  # Sample 10000 2D points
         all_datapoints = all_datapoints * 2.2 - 1.1  # Scale from -1.1 to 1.1
         x_df = pd.DataFrame(all_datapoints, columns=["X", "Y"])
-        validity_mask = self.validity(x_df)
+        validity_mask = np.logical_and(self.validity(x_df), self.secret_validity(x_df))
         y_df = pd.DataFrame(validity_mask, columns=["O1"])
+        x_df = x_df[validity_mask]
+        y_df = y_df[validity_mask]
         all_df = pd.concat([x_df, y_df], axis=1)
         v = 100 * np.mean(all_df["O1"])
         print(f"{v}% of the points are valid")
@@ -127,18 +112,35 @@ class SimpleAblation2Dimensions:
                                         prediction_function=self.validity,
                                         constraint_functions=[])
 
+        features_desired = not ablation
+
+        problem.set_desired_scores(
+            gower=features_desired,
+            average_gower=features_desired,
+            change_feature_ratio=features_desired
+        )
+
         generator = CounterfactualsGenerator(problem=problem,
                                              pop_size=100,
                                              initialize_from_dataset=True)
+        generator.use_empty_repair(ablation)
         generator.generate(n_generations=100)
-        counterfactuals = generator.sample_with_dtai(num_samples=10, gower_weight=1,
-                                                     avg_gower_weight=0.5, cfc_weight=0.5,
+        counterfactuals = generator.sample_with_dtai(num_samples=50, gower_weight=0.5,
+                                                     avg_gower_weight=10, cfc_weight=0.5,
                                                      diversity_weight=0.2, include_dataset=False,
                                                      num_dpp=10000)
-        self.plotcfs(self.validity,
-                     counterfactuals.values,
-                     (0, 0),
-                     np.array([[-1.1, 1.1], [-1.1, 1.1]]))
+        self.plot_cfs(counterfactuals.values,
+                      (0, 0),
+                      np.array([[-1.1, 1.1], [-1.1, 1.1]]))
+        if ablation:
+            name = 'figure-classical.png'
+        else:
+            name = 'figure-mcd.png'
+        validity = self.secret_validity(counterfactuals)
+
+        fraction_valid = np.sum(validity) / len(validity)
+        print(f'Fraction valid: {fraction_valid}')
+        plt.savefig(name)
 
 
 if __name__ == '__main__':
